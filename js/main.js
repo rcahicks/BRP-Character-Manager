a0f34601cfef7fa2556d5988da53dc4e1c9acf24
Christian Hicks <r.ca.hicks@gmail.com>
2024-05-08 08:31:35 -0400

    Update layout, theme

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function(req, res) {
+    res.writeHead(200);
+    res.end("BRP Game Manager");
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,53 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     // colors: {
-    //   "of-green": "#81B29A",
-    //   "of-grain": "#F2CC8F",
-    //   "of-blue": "#3D405B",
-    //   "of-salmon": "#E07A5F",
-    //   "of-eggshell": "#F4F1DE"
+      
     // },
     extend: {
       colors: {
-        "of-green": "#81B29A",
-        "of-grain": "#F2CC8F",
-        "of-blue": "#3D405B",
-        "of-salmon": "#E07A5F",
-        "of-eggshell": "#F4F1DE"
-        // after we finalize colors, remove this from extended and uncoment colors.
+        "of-black": "#1A180C",
+      "of-white": "#FAFAFA",
+      },
+      backgroundImage: {
+        "gm-tools": "url('/src/images/gm-tools-bg.png')",
+      },
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 0%' },
+          '10%': { backgroundPosition: '20% 100%' },
+          '20%': { backgroundPosition: '40% 0%' },
+          '30%': { backgroundPosition: '60% 100%' },
+          '40%': { backgroundPosition: '80% 0%' },
+          '50%': { backgroundPosition: '100% 100%' },
+          '60%': { backgroundPosition: '80% 0%' },
+          '70%': { backgroundPosition: '60% 100%' },
+          '80%': { backgroundPosition: '40% 0%' },
+          '90%': { backgroundPosition: '20% 100%' },
+          '100%': { backgroundPosition: '0% 0%'},
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 2s ease infinite alternate',
       },
     },
   },
